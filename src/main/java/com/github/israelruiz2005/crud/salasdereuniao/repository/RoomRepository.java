ae84e7638c7ebd7188b09e5742d9e232ec2651fd
israelruiz2005 <israelruiz2005@gmail.com>
2021-12-20 21:31:52 -0300

    Envia parte aplicação api rest

---
+++ src/main/java/com/github/israelruiz2005/crud/salasdereuniao/repository/RoomRepository.java
@@ -0,0 +1,11 @@
+package com.github.israelruiz2005.crud.salasdereuniao.repository;
+
+import com.github.israelruiz2005.crud.salasdereuniao.model.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends JpaRepository<Room, Long>{
+
+
+}
